ffbccad68db1eca94f610a4b2a4fa41052c857ed
sara saleh alofi <saraalawfi1995@gmail.com>
2019-02-20 16:05:34 +0300

    Updates

---
+++ precache-manifest.1e714d3cb4f03ed40779803a57566c84.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "bc56e4da6778d8647d8aaf98c2d083b0",
+    "url": "/TodoList/static/media/backgroundImage.bc56e4da.gif"
+  },
+  {
+    "revision": "0e768dfdad8babe3b0c3",
+    "url": "/TodoList/static/js/runtime~main.0e768dfd.js"
+  },
+  {
+    "revision": "684a3d28cd6abe310612",
+    "url": "/TodoList/static/js/main.684a3d28.chunk.js"
+  },
+  {
+    "revision": "81c499f7533bac6c627e",
+    "url": "/TodoList/static/js/2.81c499f7.chunk.js"
+  },
+  {
+    "revision": "684a3d28cd6abe310612",
+    "url": "/TodoList/static/css/main.e1a09ce0.chunk.css"
+  },
+  {
+    "revision": "81c499f7533bac6c627e",
+    "url": "/TodoList/static/css/2.351e0143.chunk.css"
+  },
+  {
+    "revision": "51e2a0910d1bef6948e241b0ca2a97cf",
+    "url": "/TodoList/index.html"
+  }
+];
